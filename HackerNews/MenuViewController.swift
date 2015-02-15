ff1d91f8ec87f3fb2ebf646632dc84ad72aad93f
Jason Cabot <jasoncabot@gmail.com>
2015-02-15 17:20:32 +0000

    Initial commit

---
+++ HackerNews/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  HackerNews
+//
+//  Created by Jason Cabot on 15/02/2015.
+//  Copyright (c) 2015 Jason Cabot. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ HackerNews/StoryListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StoryListViewController.swift
+//  HackerNews
+//
+//  Created by Jason Cabot on 15/02/2015.
+//  Copyright (c) 2015 Jason Cabot. All rights reserved.
+//
+
+import UIKit
+
+class StoryListViewController: UIViewController {
+    
+}
