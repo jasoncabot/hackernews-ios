c2b7abe6b1ea8d1b3140836eeb8ff8b756ac6528
Jason Cabot <jasoncabot@gmail.com>
2015-02-22 12:35:58 +0000

    Added in the link to github

---
+++ HackerNews/ExternalURLSegue.swift
@@ -0,0 +1,17 @@
+
+//
+//  ExternalURLSegue.swift
+//  HackerNews
+//
+//  Created by Jason Cabot on 22/02/2015.
+//  Copyright (c) 2015 Jason Cabot. All rights reserved.
+//
+
+import UIKit
+
+class ExternalURLSegue : UIStoryboardSegue {
+    
+    override func perform() {
+        UIApplication.sharedApplication().openURL(NSURL(string: "https://github.com/jasoncabot/hackernews-ios")!)
+    }
+}
